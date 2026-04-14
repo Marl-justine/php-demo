a3cf7652f8a1a42d4809614d0d1ff4e84fedf1f2
diryujin <ujindulanas123@gmail.com>
2026-04-14 22:45:55 +0800

    feat: added labs 1-3

---
+++ lab2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <title>Lab 1</title>
+</head>
+<body>
+  <h1>This is my Lab 2</h1>
+  <h2>Welcome to Lab 2</h2>
+  <h3>This is my program</h3>
+</body>
+</html>
